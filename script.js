49ba063a17506c638b0d5c458f37c7eec63b4c02
user.Oleksandr <user.oleksandr@gmail.com>
2022-12-25 16:43:58 +0200

    commit_3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Завдання 1:
+
 /*let array = [
     5, 66, 'pool', 7, 99, null, { name: 'Sam' }, [], true, 78
 ];
@@ -7,7 +9,9 @@ function calc() {
 calc();*/
 
 
-const x = prompt('Enter your first number');
+//Завдання 2:
+
+/*const x = prompt('Enter your first number');
 const y = prompt('Enter your second number');
 const znak = prompt(`what to do: + - * / % ^`);
 
@@ -16,7 +20,34 @@ function doMath(x, znak, y) {
   return eval(x + znak.replace('^', '**') + y)
 };
 
-alert(doMath(x, znak, y));
+alert(doMath(x, znak, y));*/
+
+
+//Завдання 3:
+
+function showArr() {
+    let arr1 = [];
+    let message1 = prompt('how many items do you want to enter main arr?');
+
+    for(let i=0; i < message1; i++) {
+        arr1[i] = prompt('enter you elements', +i); 
+        };
+
+    let arr2 = [];
+    let message2 = prompt('how many items do you want to enter second arr?');
+
+    for(let i=0; i < message2; i++) {
+        arr2[i] = prompt('enter you elements', +i); 
+        };
+
+    Array.prototype.push.apply(arr1, arr2);
+    console.log(arr1);
+};
+showArr();
+
+
+
+
 
 
 
