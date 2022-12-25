aed1af07e72031273d3098f1b1b79c6b6bcb8050
user.Oleksandr <user.oleksandr@gmail.com>
2022-12-25 15:38:16 +0200

    commit_2

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-let array = [
+/*let array = [
     5, 66, 'pool', 7, 99, null, { name: 'Sam' }, [], true, 78
 ];
 function calc() {
     console.log((5 + 66 + 7 + 99 + 78) / 5)
 }
-calc();
+calc();*/
+
+
+const x = prompt('Enter your first number');
+const y = prompt('Enter your second number');
+const znak = prompt(`what to do: + - * / % ^`);
+
+function doMath(x, znak, y) {
+  
+  return eval(x + znak.replace('^', '**') + y)
+};
+
+alert(doMath(x, znak, y));
+
+
+
+
+
+
+
+
+
+
+  
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
